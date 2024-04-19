fdafc3a1e50d27b88bbc5f2753b1818d5d8910f5
Sebastien Siva <ssiva@ggc.edu>
2024-04-19 08:46:52 -0400

    Lecture 4_18_2024

--- Assets/Scripts/NetMan.cs
+++ Assets/Scripts/NetMan.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
 public class NetMan : MonoBehaviourPunCallbacks
 {
     public GameObject playerPrefab;
+    public GameObject ballPrefab;
     Spinner lavaBlade;
 
     // Start is called before the first frame update
@@ -30,5 +32,17 @@ public class NetMan : MonoBehaviourPunCallbacks
             new Vector3(Random.Range(-8, 8), Random.Range(-4, 4), 0),
             Quaternion.identity);
 
-    }    
+    }
+
+
+    // Runs once for player on each laptop
+    public override void OnPlayerEnteredRoom(Player newPlayer) {
+        //server owns the ball
+        if(PhotonNetwork.IsMasterClient) {
+            if(PhotonNetwork.CurrentRoom.PlayerCount == 4) {
+                PhotonNetwork.InstantiateRoomObject(ballPrefab.name,
+                    Vector3.zero, Quaternion.identity);
+            }
+        }
+    } 
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,13 @@ using Photon.Pun;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviourPunCallbacks
+public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
 {
     Rigidbody myBod;
     float moveForce = 500;
     Text namePlate;
+    public int health;
+    Transform greenHealth;
 
     // Start is called before the first frame update
     void Start()
@@ -16,16 +18,49 @@ public class PlayerController : MonoBehaviourPunCallbacks
         myBod = GetComponent<Rigidbody>();
         namePlate = GetComponentInChildren<Text>();
         namePlate.text = photonView.Owner.NickName;
+        greenHealth = transform.Find("Canvas/GreenHealth");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //this code runs on every cube
+        if(health > 0) {
+            greenHealth.localScale = new Vector3(health/100f, 1, 1);
+        }
+        else {
+            //die
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                5);
+            myBod.constraints = RigidbodyConstraints.FreezeAll;
+            namePlate.color = Color.gray;    
+        }
+
+        //this code runs only on the cube I own
         if(photonView.IsMine) {
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
             Vector3 f = new Vector3(h, v, 0);
             myBod.AddForce(f * moveForce * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerStay(Collider other) {
+        if(photonView.IsMine) {
+            //only owner can correclty calc physics
+            health -= 1;
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(photonView.IsMine) {
+            stream.SendNext(health);
+        } 
+        else {
+            health = (int) stream.ReceiveNext();
         }
     }
 }
